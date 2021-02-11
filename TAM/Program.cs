5a06a232912829a339dc4e10cf9bf805f714b7d3
Zachary McNaney <zmcnaney@gmail.com>
2021-02-10 20:06:40 -0500

    Initial Commit

--- TAM/Program.cs
+++ TAM/Program.cs
@@ -1,48 +1,55 @@
-﻿using Microsoft.Extensions.CommandLineUtils;
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TAM
 {
-    class Program
+
+    [Command(Name = "TAM", Description = "A list of commands to help make a TAM's life easier"),
+        Subcommand(typeof(ConfigManager))]
+        
+    [HelpOption("-?|-h|--help")]
+    public class TAM
     {
-        static void Main(string[] args)
+        public static void Main(string[] args) {
+
+
+            SetupSimpleConfiguration();
+
+            // Create service provider
+            // Print connection string to demonstrate configuration object is populated
+            Console.WriteLine(Config.GetConnectionString("DataConnection"));
+
+
+            //console.WriteLine("You must specify at a subcommand.");
+            CommandLineApplication.Execute<TAM>(args);
+                }
+
+        private static void SetupSimpleConfiguration()
         {
-            var app = new CommandLineApplication();
-            app.Name = "TAM Tools";
-            app.Description = "A series of tools which can be used to help the TAM team run smoother and automate parts of their life";
-
-            app.HelpOption("-?|-h|--help");
-
-            app.OnExecute(() =>
-            {
-                app.ShowHint();
-                return 0;
-            });
-
-            app.Command("refresh", (command) =>
-            {
-                command.Description = "Refresh local system";
-
-
-                command.HelpOption("-?|-h|--help");
-
-                command.OnExecute(() =>
-                {
-                    Console.WriteLine("simple-command has finished.");
-                    return 0;
-                });
-            });
-
-            try
-            {
-                app.Execute(args);
-            }
-            catch (CommandParsingException ex)
-            {
-                Console.WriteLine(ex.Message);
-                app.ShowHelp();
-            }
-
+            Config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("app.json")
+                .Build();
         }
+        public static IConfigurationRoot Config { get; private set; }
+
+        private int OnExecute(CommandLineApplication app, IConsole console)
+        {
+            console.WriteLine("You must specify at a subcommand.");
+            app.ShowHelp();
+            return 1;
+        }
+
+
     }
+
 }
+    
